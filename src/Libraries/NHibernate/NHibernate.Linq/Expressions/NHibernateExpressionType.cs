bb4965a59a8c8ed468d8bfb6bd093447a0958b03
Ubuntu <ubuntu@ip-10-240-132-144.us-west-2.compute.internal>
2024-12-05 08:11:06 +0000

    Remove unnecessary Orchard and System.Web.Mvc dependencies from NHibernate.Linq Expression files

--- src/Libraries/NHibernate/NHibernate.Linq/Expressions/NHibernateExpressionType.cs
+++ src/Libraries/NHibernate/NHibernate.Linq/Expressions/NHibernateExpressionType.cs
@@ -1,23 +1,14 @@
-using Orchard.ContentManagement;
-using Orchard.Security;
-using Orchard.UI.Admin;
-using Orchard.DisplayManagement;
-using Orchard.Localization;
-using Orchard.Services;
-using System.Web.Mvc;
-using Orchard.Mvc.Filters;
-
 namespace NHibernate.Linq.Expressions
 {
-	/// <summary>
-	/// Extended node types for custom expressions
-	/// </summary>
-	public enum NHibernateExpressionType
-	{
-		QuerySource = 1000, //make sure these don't overlap with ExpressionType
-		RootEntity,
-		Entity,
-		PropertyAccess,
-		CollectionAccess
-	}
+    /// <summary>
+    /// Extended node types for custom expressions
+    /// </summary>
+    public enum NHibernateExpressionType
+    {
+        QuerySource = 1000, //make sure these don't overlap with ExpressionType
+        RootEntity,
+        Entity,
+        PropertyAccess,
+        CollectionAccess
+    }
 }
--- src/Libraries/NHibernate/NHibernate.Linq/Expressions/QuerySourceExpression.cs
+++ src/Libraries/NHibernate/NHibernate.Linq/Expressions/QuerySourceExpression.cs
@@ -1,12 +1,4 @@
-using Orchard.ContentManagement;
-using Orchard.Security;
-using Orchard.UI.Admin;
-using Orchard.DisplayManagement;
-using Orchard.Localization;
-using Orchard.Services;
-using System.Web.Mvc;
-using Orchard.Mvc.Filters;
-﻿using System;
+using System;
 using System.Linq;
 
 namespace NHibernate.Linq.Expressions
